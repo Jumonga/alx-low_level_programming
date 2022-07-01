53c7fd1c549d8339c1487b6676273fb979da714d
Jeremiah <toluakintola0@gmail.com>
2022-06-30 18:15:46 -0700

    Done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - part of the program
+ *
+ * Preparing to retun zero while it works
+ */
+ 
+ int  main(void)
+ {
+ 	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+ }
