fae574c5ec4b2820afbdb652061f9c0d8c15aada
Jeremiah <toluakintola0@gmail.com>
2022-07-01 12:36:52 -0700

    There again

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- *Code runs here for 
- *printing grammar
-*/
-
-/*Main program starts here*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
-{	
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-/*End the program here*/
 	return (0);
-	}
+}
