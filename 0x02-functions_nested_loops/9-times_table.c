bfc19337febbed04e2797edb7f307685cd2e167d
Jeremiah <toluakintola0@gmail.com>
2022-07-05 13:40:10 -0700

    Not yet

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,15 +9,15 @@
 
 void times_table(void)
 {
-	int a, x, prod;
+	int y, x, prod;
 
 	prod = 0;
 
-	for (a=0; a <= 9; a++)
+	for (y = 0; y <= 9; x++)
 	{
-		for (x=0; x <= 9; x++)
+		for(x = 0; x <= 9; x++)
 		{
-			prod  = a * x;
+			prod = y * x;
 			if (x != 0)
 			{
 				_putchar(',');
@@ -27,16 +27,16 @@ void times_table(void)
 			{
 				_putchar((prod / 10) + '0');
 				_putchar((prod % 10) + '0');
-
 			}
 			else if ((prod < 10) && (x != 0))
 			{
 				_putchar(' ');
-				_putchar((prod % 10) + '0';
+				_putchar((prod % 10) + '0');
 			}
-			else
+			else	
 				_putchar((prod % 10) + '0');
 		}
+
 		_putchar('\n');
 	}
 }
