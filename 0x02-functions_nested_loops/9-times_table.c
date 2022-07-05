aa0663cd59082b2f772d065f20fe85e0370cbc10
Jeremiah <toluakintola0@gmail.com>
2022-07-05 13:31:00 -0700

    Done another one

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * times_table - This prints times table up to 9
+ *
+ * Return: void
+ *
+ **/
+
+void times_table(void)
+{
+	int a, x, prod;
+
+	prod = 0;
+
+	for (a=0; a <= 9; a++)
+	{
+		for (x=0; x <= 9; x++)
+		{
+			prod  = a * x;
+			if (x != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (prod >= 10)
+			{
+				_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
+
+			}
+			else if ((prod < 10) && (x != 0))
+			{
+				_putchar(' ');
+				_putchar((prod % 10) + '0';
+			}
+			else
+				_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
